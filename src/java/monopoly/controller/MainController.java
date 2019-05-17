970184cb159e3bfa9b38eaa2c5a0f9c97ea03240
Miguel Flores <mky.flores@hotmail.com>
2019-05-17 14:26:02 -0500

    Fixes to onAction commands

--- src/java/monopoly/controller/MainController.java
+++ src/java/monopoly/controller/MainController.java
@@ -1,9 +1,11 @@
 package monopoly.controller;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,14 +15,28 @@ public class MainController implements Initializable
     @FXML
     private ImageView imageView;
 
+    @FXML
+    private AnchorPane problemPane;
+
+    @FXML
+    private void close()
+    {
+        problemPane.setVisible(false);
+    }
+
+    @FXML
+    private void setQuestion(MouseEvent event)
+    {
+        System.out.println(event.getSource().toString());
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-        URL url = getClass().getClassLoader().getResource("monopoly/Board.png");
+//        URL url = getClass().getClassLoader().getResource("monopoly/Board.png");
 
-        Image image = new Image(url.toString());
-
-        imageView.setImage(image);
+//        Image image = new Image(url.toString());
+//        imageView.setImage(image);
         imageView.setPreserveRatio(true);
         imageView.setFitHeight(720);
         imageView.setFitWidth(720);
